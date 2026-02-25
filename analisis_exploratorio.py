ecb79323cd1b888fc1eef772debb66e6bc0d5519
elciclon <adrianfernandezfazio@gmail.com>
2026-02-25 18:55:19 -0300

    Empezando a graficar las letras para determinar atributos prescindibles

--- analisis_exploratorio.py
+++ analisis_exploratorio.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+
 letras_df = pd.read_csv('TP02-EnglishTypeAlphabet.csv')
 
 #%% Ejercicio 1, análisis exploratorio
@@ -38,12 +39,35 @@ primera_variante_por_letra = {}
 letras = "abcdefghijklmnopqrstuvwxyz"
 
 j = 0
-for i in range(0,1016,1016):
+
+#Este loop popula el diccionario con la primera variante de cada letra
+for i in range(0,26416,1016):
     primera_variante_por_letra[letras[j]] = i
     j += 1
 
-letras_df_sin_label = letras_df.drop(['label'], axis=1) 
-img = np.array(letras_df_sin_label.iloc[12]).reshape((28,28))
-plt.imshow(img, cmap='gray')
-plt.show()
+letras_df_sin_label = letras_df.drop(['label'], axis=1) #Necesario para reshape 
+#Graficamos 5 variantes por letra al azar
+for i in range(0, 26416, 1016):
+    # 1. Creamos la figura con 5 subplots (1 fila, 5 columnas)
+    fig, axes = plt.subplots(1, 5, figsize=(15, 3))
+    lista_aleatoria = np.random.randint(i, i + 1016, size=5)
+    
+    for idx_plot, j in enumerate(lista_aleatoria):
+        img = np.array(letras_df_sin_label.iloc[j]).reshape((28,28))
+        
+        # Dibujamos en el subplot correspondiente
+        axes[idx_plot].imshow(img, cmap='gray')
+        
+    plt.show() # Muestra el gráfico con las 5 letras juntas
+    
 
+#%% 
+"""Graficamos una letra de cada clase para pensar qué atributos corresponden 
+al 'marco' de pixeles irrelevantes"""
+for i in range(0, 26416, 1016):
+    # fig = plt.subplots(1, 1, figsize=(8, 8))
+    img = np.array(letras_df_sin_label.iloc[i]).reshape((28,28))
+    plt.imshow(img, cmap='gray')
+    plt.show()
+
+    
